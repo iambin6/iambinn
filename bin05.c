7b07854643764914d3111ae8b7a8a75ded5ee9da
iambin6 <miraisan2512@gmail.com>
2024-07-03 13:30:36 +0700

    Update bin05.c

--- bin05.c
+++ bin05.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-
 int main() {
   int n;
   printf("Enter n: ");
   scanf("%d", &n);
-
   int array[n];
   printf("");
   for (int i = 0; i < n; i++) {
     scanf("%d", &array[i]);
   }
-
   printf("OUTPUT:\n");
-
   printf("Odd Numbers: ");
   for (int i = 0; i < n; i++) {
     if (array[i] % 2 != 0) {
@@ -20,7 +16,6 @@ int main() {
     }
   }
   printf("\n");
-
   printf("Even Numbers: ");
   for (int i = 0; i < n; i++) {
     if (array[i] % 2 == 0) {
@@ -28,6 +23,5 @@ int main() {
     }
   }
   printf("\n");
-
   return 0;
 }
