87d34888b0afb1e2c154c20c5a098530c6931a65
iambin6 <miraisan2512@gmail.com>
2024-07-03 13:29:47 +0700

    Add files via upload

---
+++ bin05.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  printf("Enter n: ");
+  scanf("%d", &n);
+
+  int array[n];
+  printf("");
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &array[i]);
+  }
+
+  printf("OUTPUT:\n");
+
+  printf("Odd Numbers: ");
+  for (int i = 0; i < n; i++) {
+    if (array[i] % 2 != 0) {
+      printf("%d ", array[i]);
+    }
+  }
+  printf("\n");
+
+  printf("Even Numbers: ");
+  for (int i = 0; i < n; i++) {
+    if (array[i] % 2 == 0) {
+      printf("%d ", array[i]);
+    }
+  }
+  printf("\n");
+
+  return 0;
+}
